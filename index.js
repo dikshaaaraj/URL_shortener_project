26d6fd7ee392832227711ae5924fe10cc1dd7e0d
dikshaaarajj <diksharaj2911@gmail.com>
2026-04-27 16:12:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const {connectToMongoDB} = require("./connect");
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/url");
+const URL = require('./models/url'); 
+const app = express();
+const PORT = 8001;
+const path = require('path');
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() => console.log("Mongodb connected")
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views")); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/test", async (req, res) =>{
+    const allUrls = await URL.find({});
+    return res.render("home",{
+        urls: allUrls
+    });
+});
+
+app.use("/url", urlRoute);
+
+app.use("/", staticRoute);
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+app.get("/:shortId",async (req, res) =>{
+   const shortId = req.params.shortId;
+  
+   const entry = await URL.findOneAndUpdate(
+    {
+        shortId,
+    },
+    {
+        $push: {
+            visitHistory: {
+                timestamp : Date.now(),
+            },
+        },
+    }
+   );
+   res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`))
